db6ab8518b6d07ce6422faf92038a0713daacfeb
한예주 <yejoo_han@college.harvard.edu>
2020-08-25 21:19:51 +0900

    make board

--- 15puzzle.cpp
+++ 15puzzle.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <random>
 using namespace std;
 
 void printboard (vector <vector <int> > &board);
+vector <vector <int> > makeboard(int rows, int cols);
 
 int main (int arg, char** argv)
 {
@@ -10,14 +12,7 @@ int main (int arg, char** argv)
   int cols = 4;
   vector <vector <int> > board (rows, vector<int> (cols));
 
-  int k = 1;
-  for (int i = 0; i < 4; i++)
-    for (int j = 0; j < 4; j++)
-    {
-      board[i][j] = k;
-      k++;
-    }
-  board[3][3] = 0;
+  board = makeboard(rows, cols);
 
   printboard(board);
 }
@@ -33,4 +28,56 @@ void printboard (vector <vector <int> > &board)
     }
     cout << endl;
   }
+}
+
+vector <vector <int> > makeboard(int rows, int cols)
+{
+  // Make an Entirely Solved Board
+  vector <vector <int> > board(rows, vector<int>(cols));
+  int num = 0;
+  for (int i = 0; i < board.size(); i++)
+    for (int j = 0; j < board[i].size(); j++)
+    {
+      num++;
+      board[i][j] = num;
+    }
+
+  int blankrow = rows - 1;
+  int blankcol = cols - 1;
+  board[blankrow][blankcol] = 0;
+
+  // Make Random generator
+  random_device device;
+  mt19937 generator(device());
+  uniform_int_distribution<> directiondis(1, 4);
+
+  // Swap for sufficient turns
+  int trials = pow(2, rows + cols);
+  for (int i = 0; i < trials; i++)
+  {
+    int direction = directiondis(generator);
+    int changerow = 0;
+    int changecol = 0;
+
+    // Swap UP
+    if (direction == 1) changerow = -1;
+    // Swap RIGHT
+    if (direction == 2) changecol = 1;
+    // Swap DOWN
+    if (direction == 3) changerow = 1;
+    // Swap LEFT
+    if (direction == 4) changecol = -1;
+
+    // If Direction is Out of Array, CONTINUE
+    if (blankrow + changerow < 0 || blankrow + changerow >= board.size()) continue;
+    if (blankcol + changecol < 0 || blankcol + changecol >= board[0].size()) continue;
+
+    // Do the SWAP and update where blank row / col is
+    board[blankrow][blankcol] = board[blankrow + changerow][blankcol + changecol];
+    blankrow = blankrow + changerow;
+    blankcol = blankcol + changecol;
+    board[blankrow][blankcol] = 0;
+  }
+
+  return board;
 }

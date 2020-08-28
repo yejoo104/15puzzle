5e811b1e2a551edb495e888c335416586dab393c
한예주 <yejoo_han@college.harvard.edu>
2020-08-28 21:14:35 +0900

    ready for graphics

---
+++ 15puzzle2.cpp
@@ -0,0 +1,14 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <random>
+using namespace sf;
+using namespace std;
+
+vector <vector <int> > makeboard(int rows, int cols);
+
+int main (int arg, char** argv)
+{
+
+}
--- makefile
+++ makefile
@@ -1,2 +1,4 @@
+15puzzle2: 15puzzle2.cpp
+	g++ -std=c++17 -o 15puzzle2 15puzzle2.cpp
 15puzzle: 15puzzle.cpp
-  g++ -std=c++17 -o 15puzzle 15puzzle.cpp -llibcurses
+  g++ -std=c++17 -o 15puzzle 15puzzle.cpp 
